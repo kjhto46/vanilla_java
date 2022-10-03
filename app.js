012a64c512d042d8e18266383ab68dc1f75ec937
권준희 <81128476+kjhto46@users.noreply.github.com>
2022-10-03 16:35:56 +0900

    4.2 수업내용

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-const loginForm = document.getElementById("login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 
-function onLoginBtnClick() {
-    // console.log(loginInput.value);
-    // console.log('click');
-
-    const username = loginInput.value;
-    // input 에서 최대 길이 설정가능
-    // if (username === "") {
-    //     console.log("이름을 작성해주세요");
-    // } else if(username.length > 15) {
-    //     alert("이름이 너무 깁니다.");
-    // }
-
-
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    console.log(loginInput.value);
+    // preventDefault(); 기본동작 막아주기
 }
 
-
-loginButton.addEventListener("click",onLoginBtnClick)
+loginForm.addEventListener("submit",onLoginSubmit);
