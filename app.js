c9279a739f5bd41a0da9fc83ec93591a1c2355de
권준희 <81128476+kjhto46@users.noreply.github.com>
2022-10-03 17:08:20 +0900

    4.4 input value 얻기

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event);
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`; // 1번 옆 ` 버튼 취향차이
+    greeting.classList.remove(HIDDEN_CLASSNAME);
     // preventDefault(); 기본동작 막아주기
 }
 
-function handleLinkClick(event){
-    event.preventDefault(); //기본동작 막음
-    console.log(event);
-    alert("클릭"); //<!-- 여기서 기본동작이 잠시 멈춘뒤 확인을 눌렀을때 다시 작동한다.
-}
 
 loginForm.addEventListener("submit",onLoginSubmit);
 link.addEventListener("click",handleLinkClick);
