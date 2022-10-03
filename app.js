b83a05d112e3a2e722c5dc4cbc6b9dae804a3198
권준희 <81128476+kjhto46@users.noreply.github.com>
2022-10-03 16:46:45 +0900

    4.3 a 태그 기본동작 막기 시험

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     console.log(event);
@@ -8,4 +10,11 @@ function onLoginSubmit(event) {
     // preventDefault(); 기본동작 막아주기
 }
 
+function handleLinkClick(event){
+    event.preventDefault(); //기본동작 막음
+    console.log(event);
+    alert("클릭"); //<!-- 여기서 기본동작이 잠시 멈춘뒤 확인을 눌렀을때 다시 작동한다.
+}
+
 loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
